3ba6d6485822d3a836ae559145e71a32d09e3eef
nataliaMarzec <https://github.com/nataliaMarzec>
2019-08-29 16:52:15 -0300

    express y body-parser en el server

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,36 @@
-var http = require("http");
+express = require("express");
+bodyParser = require("body-parser");
 
-function init(home, id_elemento) {
-  console.log("Loading server");
-  
-  http
-    .createServer(function(request, response) {
-      console.log(`Request received, `);
-      elemento = home.get(id_elemento)
-      elemento.precio = elemento.precio + 10
-      home.update(elemento)
+function init(home) {
+  var server = express();
+  server.use(bodyParser.json());
 
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.write(JSON.stringify(elemento));
-      response.end();
-    })
-    .listen(8888);
+  server.get("/", (req, res) => {
+    res.json(home.all());
+  })
+
+  server.get("/:id", (req, res) => {
+    res.json(home.get(req.params.id));
+  })
+
+  server.put("/", (req, res) => {
+    home.update(req.body);
+    res.status(204).end();
+  })
+
+  server.post("/", (req, res) => {
+    home.insert(req.body);
+    res.status(204).end();
+  })
+
+  server.delete("/:id", (req, res) => {
+    home.delete(req.params.id);
+    res.status(204).end();
+  });
+
+  server.listen(8888, () => {
+    console.log("Server running on port 8888");
+  });
 }
 
 exports.init = init;
-
-
-
